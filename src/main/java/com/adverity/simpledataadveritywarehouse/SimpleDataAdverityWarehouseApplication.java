2dd8139fde6026e4e8bea2dcb3c54e09dc95d9ae
szczepankras <szczepankras@gmail.com>
2021-06-25 16:55:41 +0200

    [Story-1] Initliazie Spring Boot Project *template code added

---
+++ src/main/java/com/adverity/simpledataadveritywarehouse/SimpleDataAdverityWarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.adverity.simpledataadveritywarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleDataAdverityWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleDataAdverityWarehouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adverity/simpledataadveritywarehouse/SimpleDataAdverityWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adverity.simpledataadveritywarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleDataAdverityWarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
