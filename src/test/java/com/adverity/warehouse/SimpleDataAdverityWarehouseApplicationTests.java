ef4bc621af18804e4c2ff42a30020d08238e700c
szczepankras <szczepankras@gmail.com>
2021-06-25 18:20:02 +0200

    [Story-2] Integrate Githhub with Heroku *fix test package name and change packaging

--- src/test/java/com/adverity/warehouse/SimpleDataAdverityWarehouseApplicationTests.java
+++ src/test/java/com/adverity/warehouse/SimpleDataAdverityWarehouseApplicationTests.java
@@ -1,4 +1,4 @@
-package com.adverity.simpledataadveritywarehouse;
+package com.adverity.warehouse;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
