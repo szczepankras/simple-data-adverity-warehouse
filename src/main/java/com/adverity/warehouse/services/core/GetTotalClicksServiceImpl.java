d827703c974fbe9999ff2984bf2a98580847f281
szczepankras <szczepankras@gmail.com>
2021-06-27 22:29:45 +0200

    [Story-5] Expose total clicks* add logs

--- src/main/java/com/adverity/warehouse/services/core/GetTotalClicksServiceImpl.java
+++ src/main/java/com/adverity/warehouse/services/core/GetTotalClicksServiceImpl.java
@@ -1,11 +1,13 @@
 package com.adverity.warehouse.services.core;
 
 import com.adverity.warehouse.repositories.CampaignMetricsRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 
 @Service
+@Slf4j
 public class GetTotalClicksServiceImpl implements GetTotalClicksService {
 
     private CampaignMetricsRepository campaignMetricsRepository;
@@ -16,6 +18,9 @@ public class GetTotalClicksServiceImpl implements GetTotalClicksService {
 
     @Override
     public Long getTotalClicks(String dataSource, LocalDate from, LocalDate to) {
-        return campaignMetricsRepository.getTotalClicksByDataSourceAndDateRange(dataSource, from, to);
+        log.info("Fetch total clicks for {} from data repository, status=started", dataSource);
+        Long result = campaignMetricsRepository.getTotalClicksByDataSourceAndDateRange(dataSource, from, to);
+        log.info("Fetch total clicks for {} from data repository, status=finished", dataSource);
+        return result;
     }
 }
