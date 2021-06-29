f37443ff8319ca619b5a720922cfdde3a6f9cfff
szczepankras <szczepankras@gmail.com>
2021-06-29 22:18:12 +0200

    [Story-22] Polish code * add unit tests with csv file

--- src/test/java/com/adverity/warehouse/services/core/extract/CSVDataParserTest.java
+++ src/test/java/com/adverity/warehouse/services/core/extract/CSVDataParserTest.java
@@ -5,9 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CSVDataParserTest {
@@ -18,6 +21,22 @@ class CSVDataParserTest {
     @BeforeEach
     void setup() {
         MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void shouldParseProperly() throws FileNotFoundException {
+        //given
+        InputStream inputStream = new FileInputStream("src/test/resources/test-data.csv");
+
+        //when
+        List<String[]> result = dataParser.parse(inputStream);
+
+        //then
+        assertEquals("Adverity Ads", result.get(2)[0]);
+        assertEquals("Reklama", result.get(2)[1]);
+        assertEquals("11/14/19", result.get(2)[2]);
+        assertEquals("147", result.get(2)[3]);
+        assertEquals("80351", result.get(2)[4]);
     }
 
     @Test
